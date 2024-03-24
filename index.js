a23de2c606ff37c098a5c86df6ff819eb6be4c4f
Meshmanuu <meshanyangu@gmail.com>
2024-03-24 16:26:37 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42); // assuming headquarters is at block 42
+  }
+  
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264; // 1 block = 264 feet
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    return Math.abs(destination - start) * 264;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let fare = 0;
+  
+    if (distance <= 400) {
+      fare = 0;
+    } else if (distance > 400 && distance <= 2000) {
+      fare = (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance < 2500) {
+      fare = 25;
+    } else if (distance >= 2500) {
+      return 'cannot travel that far';
+    }
+  
+    return fare;
+  }
+  
